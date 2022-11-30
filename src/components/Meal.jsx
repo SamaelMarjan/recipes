7b3f64ae670f3e5cc02c50b756a43e6f93e956f8
Samael Marjan <samaaelmarjan@gmail.com>
2022-11-30 04:23:19 -0500

    main2

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import MealItem from './MealItem';
+import RecipeIndex from './RecipeIndex';
 
 const Meal = () => {
+    const [url, setUrl] = useState('https://www.themealdb.com/api/json/v1/1/search.php?f=a');
+
+    const [item, setItem] = useState();
+    const [show, setShow] = useState(false);
+
+    useEffect (() => {
+        fetch(url).then(res => res.json()).then(data => {
+            console.log(data.meals);
+            setItem(data.meals);
+            setShow(true);
+        })
+    }, [url])
+
   return (
     <>
         <div className='main'>
@@ -11,9 +26,17 @@ const Meal = () => {
             <div className='searchBox'>
                 <input type='search' className= 'search-bar' />
             </div>
+            <div className='container'>
+                {
+                    show ? <MealItem data={item} /> : 'Not Found'
+                }
+            </div>
+            <div className="indexContainer">
+                <RecipeIndex />
+            </div>
         </div>
     </>
-  )
+  );
 }
 
-export default Meal
+export default Meal;
